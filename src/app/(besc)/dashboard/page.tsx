5cc0f281e758fa3b0f723122fd86f0c037fd8e61
desaiHarsh08 <desaiharsh183@gmail.com>
2025-03-03 14:25:24 +0530

    added student-fetched api

--- src/app/(besc)/dashboard/page.tsx
+++ src/app/(besc)/dashboard/page.tsx
@@ -1,5 +1,6 @@
 // import { mysqlConnection } from "@/db";
-import { findStudentByEmail } from "@/lib/data/student";
+import { findStudentByEmail } from "@/lib/services/student";
+import Image from "next/image";
 import React from "react";
 
 export default async function HomePage() {
@@ -7,6 +8,17 @@ export default async function HomePage() {
   //     "SELECT * FROM studentpersonaldetails WHERE id = 1"
   //   );
   const student = await findStudentByEmail("0103123014@thebges.edu.in");
+
   console.log(student);
-  return <div>HomePage- {student?.name}</div>;
+  return (
+    <div>
+      HomePage- {student?.name}
+      <Image
+        src={`https://74.207.233.48:8443/hrclIRP/studentimages/${student?.imgFile}`}
+        alt={student?.name || "student-profile-image"}
+        width={500}
+        height={500}
+      />
+    </div>
+  );
 }
--- src/lib/services/batch.ts
+++ src/lib/services/batch.ts
@@ -5,7 +5,11 @@ export async function findAllBatches(page: number = 1, pageSize: number = 10) {
     const offset = (page - 1) * pageSize; // Starting point for pagination
 
     const [rows] = await mysqlConnection.query<RowDataPacket[]>(
-        `SELECT * FROM studentpaperlinkingmain ORDER BY id DESC LIMIT ? OFFSET ?`,
+        `SELECT * 
+        FROM studentpaperlinkingmain 
+        ORDER BY id DESC 
+        LIMIT ? 
+        OFFSET ?`,
         [pageSize, offset]
     );
 
