d3a53780ca8f1768bb8a66a8c7383ad9b76ec88b
desaiHarsh08 <desaiharsh183@gmail.com>
2025-05-13 23:32:45 +0530

    added library

---
+++ src/types/academics/library.ts
@@ -0,0 +1,49 @@
+export interface IssuedBookDetails {
+    // Book Details
+    mainTitle: string;
+    subTitle: string | null;
+    alternateTitle: string | null;
+    isbn: string;
+    edition: string;
+    editionYear: string;
+    bookVolume: string | null;
+    bookPart: string | null;
+    libMasterName: string;
+    subjectGroupId: number;
+    subjectgroupName: string;
+    generalSubject: string;
+    languageName: string;
+    callNo: string;
+    publisherName: string;
+    publisherAddress: string;
+
+    // Issued Details
+    borrowingTypeId: number;
+    borrowingTypeName: string;
+    issueDate: string;
+    returnDate: string;
+    actualRetDate: string | null;
+    isReturn: boolean;
+    fine: number;
+    reIssue: boolean;
+    isForceIssue: boolean;
+
+    // Copy Details
+    copyId: number;
+    copyTypeId: number;
+    issueType: string;
+    statusId: number;
+    statusName: string;
+    rackId: number | null;
+    rackName: string | null;
+    selfId: number | null;
+    shelfName: string | null;
+    encloserTypeId: number | null;
+    enclosetypeName: string | null;
+    noOfEncloser: number | null;
+    noOfPages: number | null;
+    priceINR: number | null;
+    bindingTypeName: string | null;
+    departmentId: number | null;
+    department: string | null;
+}
