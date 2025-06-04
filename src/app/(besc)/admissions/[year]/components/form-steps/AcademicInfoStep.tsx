4becca025ad5db8f3f2d1f4ef25c8a54b202371d
desaiHarsh08 <desaiharsh183@gmail.com>
2025-06-04 19:49:53 +0530

    fixed lint

--- src/app/(besc)/admissions/[year]/components/form-steps/AcademicInfoStep.tsx
+++ src/app/(besc)/admissions/[year]/components/form-steps/AcademicInfoStep.tsx
@@ -1,5 +1,4 @@
 import { FormData } from "../../types";
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import {
